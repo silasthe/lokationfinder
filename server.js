f0d2995adaa085f77047923f137e1b46b002d6df
silasthe <146361976+silasthe@users.noreply.github.com>
2025-05-04 12:55:32 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const lobbies = {};
+
+io.on('connection', (socket) => {
+    socket.on('joinLobby', ({ lobbyId, userName, isHost }) => {
+        socket.join(lobbyId);
+        socket.lobbyId = lobbyId;
+        socket.userName = userName;
+        socket.isHost = isHost;
+        socket.updateInterval = 5000;
+
+        if (!lobbies[lobbyId]) lobbies[lobbyId] = {};
+        lobbies[lobbyId][socket.id] = { name: userName, location: null, interval: 5000 };
+
+        io.to(lobbyId).emit('userList', lobbies[lobbyId]);
+    });
+
+    socket.on('locationUpdate', (location) => {
+        const lobby = lobbies[socket.lobbyId];
+        if (lobby && lobby[socket.id]) {
+            lobby[socket.id].location = location;
+            io.to(socket.lobbyId).emit('userList', lobby);
+        }
+    });
+
+    socket.on('setUpdateInterval', (interval) => {
+        if (socket.isHost) {
+            for (const id in lobbies[socket.lobbyId]) {
+                lobbies[socket.lobbyId][id].interval = interval;
+            }
+            io.to(socket.lobbyId).emit('updateInterval', interval);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        const lobby = lobbies[socket.lobbyId];
+        if (lobby) {
+            delete lobby[socket.id];
+            io.to(socket.lobbyId).emit('userList', lobby);
+        }
+    });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Server kører på port ${port}`));
